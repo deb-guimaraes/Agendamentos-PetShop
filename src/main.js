d5f6e0ca6971db77528a2143f322c51dfed19a77
deb-guimaraes <deb.guimaraes7@gmail.com>
2025-06-23 22:33:33 -0300

    feat: main page imports

---
+++ src/main.js
@@ -0,0 +1,28 @@
+"use strict"
+
+//page
+import "./page-load.js"
+
+//modal
+import "./form-modal/modal_open-close"
+import "./form-modal/modal-submit.js"
+
+//libs
+import "./libs/dayjs.js"
+
+//services
+import "./services/api-config.js"
+import "./services/schedule-fetch-by-day.js"
+import "./services/schedule-new.js"
+import "./services/schedule-delete.js"
+
+//schedules
+import "./schedules/hours-list.js"
+import "./schedules/schedules-show.js"
+import "./schedules/schedules-load.js"
+import "./schedules/date-change.js"
+
+
+
+
+
